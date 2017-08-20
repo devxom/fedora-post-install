bb9c7a99671b05953c156fa7cd8db76d302864c3
Ilya Reshetnikov <devxom@gmail.com>
2017-08-20 15:47:21 +0300

    [Feature] Add helper function for output in console.
    Changes:
     - Add file with helper functions for output text in console
     - Add variables contain full path to main scripts
     - Add sourcing functions in main file
    
    GitHub-Issue: #1
    Closes: #1
    
    Signed-off-by: Ilya Reshetnikov <devxom@gmail.com>

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#
+# Author: Ilya Reshetnikov <devxom@gmail.com>
+#
+# Description:
+#   Post-instalation scripts for Fedora 25+
+
+# Define based variable
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Import Functions
+source "$DIR/function/log-helpers"
