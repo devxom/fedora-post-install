86dfab7e83fea7b6eac2c2c06b7e4582d095d5fa
Ilya Reshetnikov <devxom@gmail.com>
2017-08-20 16:18:59 +0300

    [Feature] Add block run script on unmaintained distributive
    Changes:
     - Add exit if curent distributive is't Fedora
     - Add variables contained curent distributive name
    
    Signed-off-by: Ilya Reshetnikov <devxom@gmail.com>

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@
 
 # Define based variable
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DISTRO="$(awk -F'=' '/^NAME=/{print$2}' /etc/os-release)"
 
 # Import Functions
 source "$DIR/function/log-helpers"
@@ -14,5 +15,11 @@ source "$DIR/function/log-helpers"
 # block run script with `sudo` privileges.
 if [ "$(whoami)" = "root" ]; then
   log_error "Please DO NOT execute there script from user who have \`root\` privilege"
+  exit 1
+fi
+
+# Block run script on unmaintained distributive
+if [ "${DISTRO}" != "Fedora" ]; then
+  log_error "Sorry, this script was written for Fedora only"
   exit 1
 fi
