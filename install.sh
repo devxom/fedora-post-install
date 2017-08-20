dbe4e448a64b4ea82d89314958380a136d650d85
Ilya Reshetnikov <devxom@gmail.com>
2017-08-20 15:59:55 +0300

    [Feature] Add block run script with `sudo` privileges.
    Changes:
     - Add check which user ran script & block when user = root
     - Add show message on usercheck
    
    GitHub-Issue: #2
    Closes: #2
    
    Signed-off-by: Ilya Reshetnikov <devxom@gmail.com>

--- install.sh
+++ install.sh
@@ -10,3 +10,9 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Import Functions
 source "$DIR/function/log-helpers"
+
+# block run script with `sudo` privileges.
+if [ "$(whoami)" = "root" ]; then
+  log_error "Please DO NOT execute there script from user who have \`root\` privilege"
+  exit 1
+fi
