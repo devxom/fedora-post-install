e34471e2b3cf85277be930a779db125cd6fc5bf3
Ilya Reshetnikov <devxom@gmail.com>
2017-08-20 16:34:50 +0300

    [Feature] Add detect current running user home directory.
    Changes:
     - Add detect current running user home directory with
     - Add vars contain current user home dir
    
    Signed-off-by: Ilya Reshetnikov <devxom@gmail.com>

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ DISTRO="$(awk -F'=' '/^NAME=/{print$2}' /etc/os-release)"
 # Import Functions
 source "$DIR/function/log-helpers"
 
-# block run script with `sudo` privileges.
+# Block run script with `sudo` privileges.
 if [ "$(whoami)" = "root" ]; then
   log_error "Please DO NOT execute there script from user who have \`root\` privilege"
   exit 1
@@ -22,4 +22,11 @@ fi
 if [ "${DISTRO}" != "Fedora" ]; then
   log_error "Sorry, this script was written for Fedora only"
   exit 1
+fi
+
+# Detect current running user home directory
+if [ -n "${SUDO_USER}" ]; then
+  HOMEDIR="$(eval echo ~"${SUDO_USER}")"
+else
+  HOMEDIR="${HOME}"
 fi
